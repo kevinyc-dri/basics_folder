d259e8dd672c0556d0d77701a78e4bf493049b62
Kevin Choi <kevinyoungchoi@gmail.com>
2021-04-12 11:57:46 -0400

    format code

--- topbooks.js
+++ topbooks.js
@@ -1,11 +1,18 @@
-let books = [{ name: 'Love and Other Pranks', rating: 3.0 }, { name: 'Clean Code', rating: 4.8 }, { name: 'How to Win a Hackathon', rating: 5.0 }]
+/*
+Write a function that takes an array of objects, books, and
+returns a new array of topBooks where books.rating >= 4.25
+*/
 
+let books = [
+  { name: 'Love and Other Pranks', rating: 3.0 },
+  { name: 'Clean Code', rating: 4.8 },
+  { name: 'How to Win a Hackathon', rating: 5.0 },
+]
 
 function returnTopBooks(books) {
-  return topBooks = books.filter((book) => book.rating >= 4.25)  
+  return topBooks = books.filter((book) => book.rating >= 4.25)
 }
 
-// Write a function that takes an array of objects, books, and returns a new array of topBooks where books.rating >= 4.25
 function returnTopBooksLongWay(books) {
   let topBooks = []
   for (let i = 0; i < books.length; i++) {
@@ -16,7 +23,7 @@ function returnTopBooksLongWay(books) {
   return topBooks
 }
 
-console.log(returnTopBooksLongWay(books))
 console.log(returnTopBooks(books))
+console.log(returnTopBooksLongWay(books))
 
 
